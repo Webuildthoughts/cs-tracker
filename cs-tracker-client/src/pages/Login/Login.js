f6bf2f32d51a502e06cec34c7773d87ba5b23187
siddhartha9890 <siddharth.a9890@gmail.com>
2021-09-17 17:13:12 +0530

    added login code

--- cs-tracker-client/src/pages/Login/Login.js
+++ cs-tracker-client/src/pages/Login/Login.js
@@ -2,36 +2,37 @@ import React, { useState } from 'react'
 import { toast } from 'react-toastify'
 
 const Login = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
+  // const [email, setEmail] = useState('')
+  // const [password, setPassword] = useState('')
+  // const [error, setError] = useState('')
 
-  const onButtonClick = (event) => {
-    event.preventDefault()
-    const user = {
-      email,
-      password,
-    }
+  // const onButtonClick = (event) => {
+    // event.preventDefault()
+  //   const user = {
+  //     email,
+  //     password,
+  //   }
 
-    console.log('User details:', user)
+  //   console.log('User details:', user)
 
-    // signin(user).then((data) => {
-    //   if (data.error) {
-    //     setError(data.error)
-    //     console.log('Error state: ', error)
-    //     toast.error(error)
-    //   } else {
-    //     toast.success('Welcome !')
-    //     setEmail('')
-    //     setPassword('')
-    //     setError('')
-    //   }
-    // })
-  }
+  //   // signin(user).then((data) => {
+  //   //   if (data.error) {
+  //   //     setError(data.error)
+  //   //     console.log('Error state: ', error)
+  //   //     toast.error(error)
+  //   //   } else {
+  //   //     toast.success('Welcome !')
+  //   //     setEmail('')
+  //   //     setPassword('')
+  //   //     setError('')
+  //   //   }
+  //   // })
+  // }
 
   return (
     <div>
       <h2>Login Page</h2>
+      <div class="g-signin2" data-onsuccess="onSignIn"></div>
     </div>
   )
 }
